0d69d891b8c21592b627d7e50a0f2bc3a1ddf7fa
Abdallah Ahmed Bestawy <109435584+Abdallahbestawy@users.noreply.github.com>
2024-04-15 05:31:55 +0200

    DelteGetStudentByIdDto

--- GraduationProject/GraduationProject.Data/obj/Debug/net6.0/GraduationProject.Data.AssemblyInfo.cs
+++ GraduationProject/GraduationProject.Data/obj/Debug/net6.0/GraduationProject.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GraduationProject.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4608157f7b5dc90f1b5ce1e7252684b7b35be859")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("GraduationProject.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GraduationProject.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
